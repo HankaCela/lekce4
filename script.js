108b05a60100bd7c42caca60def54b02ea9c951d
Hanka Celá <hana-cela@email.cz>
2024-10-14 18:45:40 +0200

    Pridavam ukol1

---
+++ script.js
@@ -0,0 +1,60 @@
+/*
+const jmeno = "   Hanka   "
+const heslo = "sskkllrr(("
+//             0123456789
+const rodneCislo = "980514/3366"
+const telCislo = "111222333"
+
+console.log(jmeno)
+console.log(jmeno.trim())
+
+//          vlastnost která vrátí počet znaků
+console.log(heslo.length) 
+console.log(jmeno.length)
+
+const castHesla = heslo.slice(0, 4)
+
+console.log(castHesla)
+
+
+console.log(heslo.indexOf("sk"))
+
+const poziceLomitka = rodneCislo.indexOf("/") + 1
+
+console.log(rodneCislo.slice(poziceLomitka, poziceLomitka + 5))
+
+console.log(jmeno.toLowerCase())
+console.log(jmeno.toUpperCase())
+
+console.log(telCislo.padStart (13, "+420"))
+console.log(telCislo.padEnd (13, "0"))
+
+
+const zprava = `
+<h1>Ahoj svete</h1>
+<p>Tve dnesni stastne cislo je ${Math.floor(Math.random() * 1000)}</p>
+
+<p>Tvoje mys je prave na souradnicich ${100 * 100}</p>
+`
+
+
+document.body.innerHTML += zprava
+
+*/
+
+
+const title = "Pán prstenů";
+
+console.log(`Počet znaků názvu: ${title.length}`);
+console.log(`Název filmu velkými písmeny: ${title.toUpperCase()}`);
+console.log(`Prvních pět písmen: ${title.slice(0, 5)}`);
+console.log(`Posledních pět písmen: ${title.slice(-5)}`);
+
+document.body.innerHTML += `
+  <p>Počet znaků názvu: ${title.length}</p>
+  <p>Název filmu velkými písmeny: ${title.toUpperCase()}</p>
+  <p>Prvních pět písmen: ${title.slice(0, 5)}</p>
+  <p>Posledních pět písmen: ${title.slice(-5)}</p>
+`;
+
+
